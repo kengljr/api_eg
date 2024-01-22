8ed20fcfb18b0dd4618ca15fb7005038c9005c2b
kengljr <keng.ljr555@gmail.com>
2024-01-22 11:24:52 +0700

    editing login function

--- Dockerfile
+++ Dockerfile
@@ -15,7 +15,7 @@ RUN pwd
 #Run Bash script for apt-get update
 RUN apt-get update
 RUN apt-get install ca-certificates apt-transport-https libnss3
-RUN apt-get nano
+RUN apt-get install nano vim
 
 #Get the google chrome Browser for Linux Server
 RUN wget -nc https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
--- backhaul_mesh/run.py
+++ backhaul_mesh/run.py
@@ -32,85 +32,14 @@ def LoginEG(_user,_password):
     time.sleep(1)
 
     if( current_url != driver.current_url):
+        driver.close()
         return True
     else :
+        driver.close()
         return False
+    
 
-if(LoginEG(Username_EG,PassWord_EG))=="pass":
-    filename = "input.txt"
-    filehandle = open(filename, 'r')
-    int_x=0
-    while True:
-        # read a single line
-        int_x=int_x+1
-        print(int_x)
-        line = filehandle.readline()
-        if not line:
-            break
-        print(line)
-        ss1=line
-    
-        ss1=str(ss1)
-        ss1=ss1.replace("['","")
-        ss1=ss1.replace("]'","")
-        
-        #//////////////////////////////////////////////////////////    
-        f = open("./url_device_info4.txt", "r",encoding="utf8")
-        url_get=f.read()
-        f.close()
-        url_get=url_get.replace("SNXXXXXXXXXX",str(ss1))       
-        driver.get(url_get)    
-        source2 = driver.page_source    
-        input_f2=str(source2)
-        #print(source2)    
-        ss1.replace("\n","")  
-        int_find_str=0
-        int_find_str = source2.find("InternetGatewayDevice")
-        adata=[]
-        model=SW=wlan1status=wlan5status=wlan6status= bsssid1=bsssid5=bsssid6=ssid6=""       
-        if int_find_str > 0:
-            source2=source2.replace('<html><head></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">','')
-            source2=source2.replace('</pre></body></html>','')
-            source2=source2.replace('<html><head></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">','')
-            source2=source2.replace('<html><head><meta name="color-scheme" content="light dark"></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">','')
-            source2=source2.replace('</pre></body></html>','')
-            #print(source2)     
-            json_data = json.loads(source2)
-            ONOFF="ONLINE"
-                
-            lentofjson=len(json_data['response']['data']['oidValues'])
-            print("________________________________________________________________") 
-            print(lentofjson)
-            print("________________________________________________________________")   
-            for x in range(19):                
-                adata.append((json_data['response']['data']['oidValues'][x]['value'])) #WLANConfiguration.5.Status
-               #adata.append((json_data['response']['data']['oidValues'][x]['value'])) #WLANConfiguration.5.Status
-           
-            
-        else:
-            print("OFFLINE")
-            ONOFF="OFFLINE"
-        
-        #////////////////report///////////////////////
-        ss1=ss1.replace('\n','')        
-        
-           
-        now = datetime.now()    
-        dt_string = now.strftime("%d/%m/%Y,%H:%M:%S")
-        adata1=str(adata)
-        adata1=adata1.replace("['","")
-        adata1=adata1.replace("]'","")
-        adata1=adata1.replace("'","")
-        adata1=adata1.replace("]","")
-        adata1=adata1.replace("[","")
-            
-        csvf=dt_string+","+ss1+","+ONOFF+","+adata1+"\n"
-        print(csvf)
-        
-        f = open("P1.csv", "a",encoding="utf8")
-        f.write(csvf)
-        f.close()     
-        
-    
-filehandle.close()
-driver.close()
+if (LoginEG(Username_EG,PassWord_EG)) :
+    print("PASS")
+else :
+    print("NOT PASS")
