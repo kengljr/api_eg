efdd9f73faa879d5f78a16cf42618db60ec83543
kengljr <keng.ljr555@gmail.com>
2024-01-22 14:45:44 +0700

    editing login function

--- backhaul_mesh/backhaul_mesh_views.py
+++ backhaul_mesh/backhaul_mesh_views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, renderer_classes
 import os
 from datetime import datetime
+import run
 
 
 @api_view(('GET',))
