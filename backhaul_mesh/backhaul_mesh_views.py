b43657f0f7ca51bd0e096bed94aa7cebfc84f060
kengljr <keng.ljr555@gmail.com>
2024-01-24 17:55:52 +0700

    editing backhaul_views_24012024

--- backhaul_mesh/backhaul_mesh_views.py
+++ backhaul_mesh/backhaul_mesh_views.py
@@ -1,6 +1,7 @@
 from rest_framework import views
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, renderer_classes
+from rest_framework import status
 import os
 from datetime import datetime
 from selenium import webdriver
@@ -21,12 +22,43 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 
 current_time = datetime.strftime(datetime.now(),"%Y%m%d")
-path=f"log/backhaul_mesh_api_{current_time}.log"
 
 Username_EG="01014209"
 PassWord_EG="Mi^mi^12345678901"
 
 driver = webdriver.Chrome(service=service,options=options)
+
+def request_checking (request):
+    response = Response()
+    if not (request):
+        response_msg = {"code":0,"msg":"Error","Value":"Request Body empty"}
+        response.data=response_msg
+        response.status_code=status.HTTP_400_BAD_REQUEST
+        return response
+    elif (request["Mesh_no"]==''):
+        response_msg = {"code":0,"msg":"Error","Value":"Missing MeshNumber"}
+        response.data=response_msg
+        response.status_code=status.HTTP_400_BAD_REQUEST
+        return response
+    elif (request["DSN"]==''):
+        response_msg = {"code":0,"msg":"Error","Value":"Missing DSN"}
+        response.data=response_msg
+        response.status_code=status.HTTP_400_BAD_REQUEST
+        return response
+    elif (int(request["Mesh_no"])>4) :
+        response_msg = {"code":0,"msg":"Error","Value":"MeshNumber not in range"}
+        response.data=response_msg
+        response.status_code=status.HTTP_400_BAD_REQUEST
+        return response
+    elif (request is None ) :
+        response_msg = {"code":0,"msg":"Error","Value":"DSN Not Found"}
+        response.data=response_msg
+        response.status_code=status.HTTP_400_BAD_REQUEST
+        return response
+    else:
+        response.data=request
+        response.status_code=status.HTTP_200_OK
+        return response
 
 def LoginEG(_user,_password):
     #Going to login page
@@ -54,7 +86,7 @@ def LoginEG(_user,_password):
             driver.add_cookie(cookie)
 
         driver.refresh()
-        time.sleep(2)
+        time.sleep(1.5)
 
     if( current_url != driver.current_url):
        # driver.close()
@@ -67,12 +99,12 @@ def device_check(backhaul_type,mesh_no,serial_number):
     checking = LoginEG(Username_EG,PassWord_EG)
     if (checking) :
         print("PASS")
-        f = open("./url_device.txt", "r",encoding="utf8")
+        f = open("/API_EG/backhaul_mesh/url_device.txt", "r",encoding="utf8")
         url_get=f.read().replace("\n","")
         url_get=url_get.replace("SNXXXXXXXXXX",serial_number)
         driver.get(url_get)
         json_text = json.loads(driver.find_element(By.XPATH,"//body/pre[@style='word-wrap: break-word; white-space: pre-wrap;']").text)
-       # print(json_text)
+        print(json_text)
         for member in json_text["response"]["data"]["oidValues"]:
             if  (backhaul_type in member["oid"]) and (f"Agent.{mesh_no}" in member["oid"]):
                 return member["value"]
@@ -91,7 +123,7 @@ def snippet_detail(request):
     if request.method == 'GET':
         return Response({"testt":"Testing"})
 
-@api_view(('GET',))
+@api_view(('GET','POST','PUT',))
 def get_mediatype_backhaul_mesh(request):
     """
     Retrieve, update or delete a code snippet.
@@ -99,40 +131,30 @@ def get_mediatype_backhaul_mesh(request):
 
     if request.method == 'GET' :
         #print(request.data)
-        log("request",request,request.data)
+        log("request",request.method,request,request.data)
         print(request.data)
-        #Called function
-        if not (request.data):
-            response_msg = {"code":0,"msg":"Error","Value":"Request Body empty"}
-        elif (request.data["Mesh_no"]==''):
-            response_msg = {"code":0,"msg":"Error","Value":"Missing MeshNumber"}
-        elif (request.data["DSN"]==''):
-            response_msg = {"code":0,"msg":"Error","Value":"Missing DSN"}
-        elif (int(request.data["Mesh_no"])>4) :
-            response_msg = {"code":0,"msg":"Error","Value":"MeshNumber not in range"}
+       
+        #Checking request.data return back response_msg and http_code
+        request_result = request_checking(request.data)
 
-        else:
+        if (request_result.status_code == status.HTTP_200_OK) :   
             result = device_check("MediaType",request.data["Mesh_no"],request.data["DSN"])
-            response_msg = {"code":0,"msg":"GETMediaTypebackhaulmesh","Value":result}
-        log("response",request,response_msg)
-        return Response(response_msg)
+            response_msg = {"code":0,"msg":"GETMediaTypebackhaulmesh(x)","Value":result}
+            status_code = request_result.status_code
+        else :
+            response_msg= request_result.data
+            status_code = request_result.status_code
+        log("response",request.method,request,response_msg)
+        
+    else :
+        log("request",request.method,request,request.data)
+        response_msg = {"code":0,"msg":"Error","Value":"Method not allow"}
+        log("response",request.method,request,response_msg)
 
-    elif request.method == 'PUT':
-        log("request",request,request.data)
-        response_msg = {"code":0,"msg":"Error","Value":"Methrod not allow"}
-        log("response",request,response_msg)
-        return Response(response_msg)
-
-    elif request.method == 'POST':
-        log("request",request,request.data)
-        response_msg = {"code":0,"msg":"Error","Value":"Methrod not allow"}
-        log("response",request,response_msg)
-        return Response(response_msg)
-
-
+    return Response(response_msg,status_code)
 
 #We need to use @api_view for each function.
-@api_view(('GET',))
+@api_view(('GET','PUT','POST',))
 def get_signal_strength_backhaul_mesh(request):
     """
     Retrieve, update or delete a code snippet.
@@ -140,23 +162,27 @@ def get_signal_strength_backhaul_mesh(request):
 
     if request.method == 'GET' :
 
-        log("request",request,request.data)
-        #Called function
-        if not (request.data):
-            response_msg = {"code":0,"msg":"Error","Value":"Request Body empty"}
-        elif (request.data["Mesh_no"]==''):
-            response_msg = {"code":0,"msg":"Error","Value":"Missing MeshNumber"}
-        elif (request.data["DSN"]==''):
-            response_msg = {"code":0,"msg":"Error","Value":"Missing DSN"}
-        elif (int(request.data["Mesh_no"])>4) :
-            response_msg = {"code":0,"msg":"Error","Value":"MeshNumber not in range"}
-        else :
-            result = device_check("SignalStrength",request.data["Mesh_no"],request.data["DSN"])
-            response_msg = {"code":0,"msg":"GETSignalStrengthbackhaulmesh(x)","Value":result}
-        log("response",request,response_msg)
-        return Response(response_msg)
+        log("request",request.method,request,request.data)
+        #Checking request.data return back response_msg and http_code
+        request_result = request_checking(request.data)
 
-@api_view(('GET',))
+        if (request_result.status_code == status.HTTP_200_OK) :   
+            result = device_check("SignalStrength",request.data["Mesh_no"],request.data["DSN"])
+            response_msg = {"code":0,"msg":"SignalStrengthbackhaulmesh(x)","Value":result}
+            status_code = request_result.status_code
+        else :
+            response_msg= request_result.data
+            status_code = request_result.status_code
+        log("response",request.method,request,response_msg)
+        
+    else :
+        log("request",request.method,request,request.data)
+        response_msg = {"code":0,"msg":"Error","Value":"Method not allow"}
+        log("response",request.method,request,response_msg)
+
+    return Response(response_msg,status_code)
+
+@api_view(('GET','PUT','POST',))
 def get_phy_rate_backhaul_mesh(request):
     """
     Retrieve, update or delete a code snippet.
@@ -164,62 +190,75 @@ def get_phy_rate_backhaul_mesh(request):
 
     if request.method == 'GET' :
 
-        log("request",request,request.data)
+        log("request",request.method,request,request.data)
         #Called function
-        if not (request.data):
-            response_msg = {"code":0,"msg":"Error","Value":"Request Body empty"}
-        elif (request.data["Mesh_no"]==''):
-            response_msg = {"code":0,"msg":"Error","Value":"Missing MeshNumber"}
-        elif (request.data["DSN"]==''):
-            response_msg = {"code":0,"msg":"Error","Value":"Missing DSN"}
-        elif (int(request.data["Mesh_no"])>4) :
-            response_msg = {"code":0,"msg":"Error","Value":"MeshNumber not in range"}
-        else :
+        request_result = request_checking(request.data)
+
+        if (request_result.status_code == status.HTTP_200_OK) :   
             result = device_check("PHYRate",request.data["Mesh_no"],request.data["DSN"])
             response_msg = {"code":0,"msg":"GETPHYRatebackhaulmesh(x)","Value":result}
-        log("response",request,response_msg)
-        return Response(response_msg)
+            status_code = request_result.status_code
+        else :
+            response_msg= request_result.data
+            status_code = request_result.status_code
+        log("response",request.method,request,response_msg)
+        
+    else :
+        log("request",request.method,request,request.data)
+        response_msg = {"code":0,"msg":"Error","Value":"Method not allow"}
+        log("response",request.method,request,response_msg)
 
-@api_view(('GET',))
+    return Response(response_msg,status_code)
+
+@api_view(('GET','PUT','POST',))
 def get_serial_number_backhaul_mesh(request):
     """
     Retrieve, update or delete a code snippet.
     """
 
     if request.method == 'GET' :
-        log("request",request,request.data)
+        log("request",request.method,request,request.data)
         #Called function
-        if not (request.data):
-            response_msg = {"code":0,"msg":"Error","Value":"Request Body empty"}
-        elif (request.data["Mesh_no"]==''):
-            response_msg = {"code":0,"msg":"Error","Value":"Missing MeshNumber"}
-        elif (request.data["DSN"]==''):
-            response_msg = {"code":0,"msg":"Error","Value":"Missing DSN"}
-        elif (int(request.data["Mesh_no"])>4) :
-            response_msg = {"code":0,"msg":"Error","Value":"MeshNumber not in range"}
+        request_result = request_checking(request.data)
+
+        if (request_result.status_code == status.HTTP_200_OK) :   
+            result = device_check("SerialNumber",request.data["Mesh_no"],request.data["DSN"])
+            response_msg = {"code":0,"msg":"GETSerialNumberbackhaulmesh(x)","Value":result}
+            status_code = request_result.status_code
         else :
-             result = device_check("SerialNumber",request.data["Mesh_no"],request.data["DSN"])
-             response_msg = {"code":0,"msg":"GETSerialNumberbackhaulmesh(x)","Value":result}
-        log("response",request,response_msg)
-        return Response(response_msg)
+            response_msg= request_result.data
+            status_code = request_result.status_code
+        log("response",request.method,request,response_msg)
+        
+    else :
+        log("request",request.method,request,request.data)
+        response_msg = {"code":0,"msg":"Error","Value":"Method not allow"}
+        log("response",request.method,request,response_msg)
+
+    return Response(response_msg,status_code)
 
 @api_view(('GET',))
-def get_log(request):
+def get_log(request,time):
+    path=f"log/backhaul_mesh_api_{time}.log"
     response_msg= open(path, "r")
-    return Response(response_msg)
+    return Response(response_msg,status=status.HTTP_200_OK)
 
 
-def log(action,request,body_detail):
+def log(action,request_method,request,body_detail):
+    current_time = datetime.strftime(datetime.now(),"%Y%m%d")
+    path=f"log/backhaul_mesh_api_{current_time}.log"
     client_ip = request.META.get('REMOTE_ADDR')
     url = request.path_info # from where url
     #body_detail = request.data #What they send or what we send
     timestamp = datetime.strftime(datetime.now(),"%Y/%m/%d %H:%M:%S")
-    log_string = f"{timestamp}|{action}|{client_ip}|{url}|{body_detail}\n"
+
+
+    log_string = f"{timestamp}|{action}|{request_method}|{client_ip}|{url}|{body_detail}\n"
     print(log_string)
     if (os.path.isfile(path)):
         file = open(path,'a')
     else :
         file = open(path,'w')
-        file.write("timestamp|action|client_ip|path|body_detail\n")
+        file.write("timestamp|action|request_method|client_ip|path|body_detail\n")
     file.write(log_string)
     file.close()
